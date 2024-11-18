a32b3c946c9442c1b2a5bd35ea5a3d30f621903d
Alessio Briga <82155493+Alexndroit@users.noreply.github.com>
2024-11-18 01:51:08 +0000

    Update database.py

--- database.py
+++ database.py
@@ -2,14 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# This is the database URL
+# Database URL
 DATABASE_URL = "sqlite:///./test.db"
 
-# This creates the database engine
+# Database engine
 engine = create_engine(DATABASE_URL)
 
-# This creates a session factory
+# Session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# This creates a base class for our models
+# Base class for the models
 Base = declarative_base()
