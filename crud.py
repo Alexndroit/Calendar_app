8783fe09d68455b91c3c33e568dd77c896435109
Alessio Briga <82155493+Alexndroit@users.noreply.github.com>
2024-11-18 01:35:49 +0000

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-# Create a new customer
+# Create new customer
 def create_customer(db: Session, customer: schemas.CustomerCreate):
     db_customer = models.Customer(name=customer.name, email=customer.email)
     db.add(db_customer)
@@ -9,11 +9,11 @@ def create_customer(db: Session, customer: schemas.CustomerCreate):
     db.refresh(db_customer)
     return db_customer
 
-# Get a customer by ID
+# Get the customer by ID
 def get_customer(db: Session, customer_id: int):
     return db.query(models.Customer).filter(models.Customer.id == customer_id).first()
 
-# Get all customers
+# Get all the customers
 def get_customers(db: Session, skip: int = 0, limit: int = 10):
     return db.query(models.Customer).offset(skip).limit(limit).all()
 
@@ -38,6 +38,6 @@ def create_address(db: Session, address: schemas.AddressCreate, customer_id: int
     db.refresh(db_address)
     return db_address
 
-# Get all addresses for a customer
+# Get all of the addresses for a customer
 def get_addresses_by_customer(db: Session, customer_id: int):
     return db.query(models.Address).filter(models.Address.customer_id == customer_id).all()
